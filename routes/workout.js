b816ea6839255993f39eec2a7f38fc1b7ce08346
vonviz <voncarlo0211@gmail.com>
2024-07-09 17:54:10 +0800

    Added Updated Code

--- routes/workout.js
+++ routes/workout.js
@@ -3,11 +3,16 @@ const workoutController = require("../controllers/workoutController");
 const { verify, isLoggedIn } = require("../auth");
 const router = express.Router();
 
-router.post("/", verify, workoutController.addWorkout);
+router.post("/addWorkout", verify, workoutController.addWorkout);
 router.get("/getMyWorkOuts", verify, workoutController.getAllWorkouts);
 // router.patch("/updateWorkout/:userId", verify, workoutController.updateWorkout);
 router.patch(
   "/updateWorkout/:workoutId",
+  verify,
+  workoutController.updateWorkout
+);
+router.patch(
+  "/completeWorkoutStatus/:workoutId",
   verify,
   workoutController.updateWorkout
 );
